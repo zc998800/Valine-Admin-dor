249df294f1d5dc597b6e722967c7348beeb6b335
dorcandy <zc998800@gmail.com>
2020-03-22 20:13:51 +0800

    dnjk

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -34,7 +34,7 @@ exports.notice = (comment) => {
         return;
     }
 
-    let emailSubject = '🎀 哇！「' + process.env.SITE_NAME + '」上有新评论啦，快点我！ 💖';
+    let emailSubject = '🚩 哇！「' + process.env.SITE_NAME + '」上有新评论啦';
     let emailContent =  noticeTemplate({
                             siteName: process.env.SITE_NAME,
                             siteUrl: process.env.SITE_URL,
@@ -70,7 +70,7 @@ exports.send = (currentComment, parentComment)=> {
         || parentComment.get('mail') === process.env.SMTP_USER) {
         return;
     }
-    let emailSubject = '🎀 咦？「' + process.env.SITE_NAME + '」上有人回复了你了哦 💖';
+    let emailSubject = '📌 哇！「' + process.env.SITE_NAME + '」上有人回复了你啦！快点我！💦';
     let emailContent = sendTemplate({
                             siteName: process.env.SITE_NAME,
                             siteUrl: process.env.SITE_URL,
